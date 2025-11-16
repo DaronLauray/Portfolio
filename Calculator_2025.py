921397fd99dd2a7ae2353d7461d5e7ae9d4f4d5f
DaronLauray <237366711+DaronLauray@users.noreply.github.com>
2025-11-16 04:14:58 +0000

    modified:   Calculator_2025.py

--- Calculator_2025.py
+++ Calculator_2025.py
@@ -1,33 +1,51 @@
 print('Calculator using Python-Daron Lauray')
 #A simple calculator that can be customized for any calculation
-value= input(' what do you want to do?: +,-,*,^ or /')
+import sys
+import math
+#sys.maxsize
+sys.set_int_max_str_digits(100000000)
 
-a= int(input('first number'))
-b= int(input('second number'))
+value=0
+while value != "done":
+    value= input(' what do you want to do?: +,-,*,^,/ or done')
+    
+    def add():  
+        print(f"answer is {(round(atot,2))}")
+    def subtract():
+        print(f"answer is {(round(stot,2))}")
+    def multiply():
+        print(f"answer is {(round(mtot,2))}")
+    def divide():
+        print(f"answer is {(round(dtot,2))}")
+    def exp():
+        print(f"answer is {(round(exptot,2))}")
 
-atot= float(a+b)
-stot= float(a-b)
-mtot= float(a*b)
-dtot= float(a/b)
-exptot= float(a**b)
-def add():  
-    print(f"answer is {round(atot,2)}")
-def subtract():
-    print(f"answer is {round(stot,2)}")
-def multiply():
-    print(f"answer is {round(mtot,2)}")
-def divide():
-    print(f"answer is {round(dtot,2)}")
-def exp():
-    print(f"answer is {round(exptot,2)}")
-if value == "+":
-    add()
-elif value == "-":
-    subtract()
-elif value == "*":
-    multiply()
-elif value == "/":
-    divide()
-elif value == "^":
-    exp()
-else: print('input the function without spaces, and only input the mode symbol')
+ 
+    a= int(input('first number'))
+    b= int(input('second number'))
+
+    atot= int(a+b)
+    stot= int(a-b)
+    mtot= int(a*b)
+    dtot= int(a/b)
+    exptot= int(a**b)
+
+   
+
+   
+    if value == "+":
+        add()
+    elif value == "-":
+        subtract()
+        
+    elif value== "*":
+        multiply()
+        
+    elif value == "/":
+        divide()
+        
+    elif value == "^":
+        exp()
+    
+    else: print('input the function without spaces, and only input the mode symbol')
+    
